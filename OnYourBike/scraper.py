baa7dbca5afdd04c5dfd5a3a925b1adb02c305bd
thanders <thanders@alumni.purdue.edu>
2018-03-19 17:27:02 +0100

    Scraper works, we need to define static and dynamic varables and get it running on EC2

--- OnYourBike/scraper.py
+++ OnYourBike/scraper.py
@@ -4,6 +4,7 @@ import urllib.request, json
 import requests
 import json
 from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
 
 import sys
 sys.path.append('.')
@@ -18,20 +19,21 @@ class Bike_scraper:
     def __main__(self, contract):
         print("main method automatically called")
         scheduler = BlockingScheduler()
-        scheduler.add_job(self.scrape(contract), 'interval', seconds=10)
+        scheduler.add_job(self.scrape, 'interval', seconds=10)
         scheduler.start()
         return 0
 
-    def scrape(self, contract):
-        print(self.apikey)
+    def scrape(self):
+        contract = self.contract
         api_url = "https://api.jcdecaux.com/vls/v1/stations?contract="+self.contract+"&apiKey="+self.apikey+""
         response = requests.get(api_url)
         print(response.status_code)
         parsed_json = json.loads(response.content)
-
+        '''
         #save to a file:
         with open('data.txt', 'w') as outfile:
             json.dump(parsed_json, outfile)
+        '''
         count = 0
         for i in parsed_json:
             number = (i['number'])
@@ -41,7 +43,8 @@ class Bike_scraper:
             lat = i['position']['lat']
             lng = i['position']['lng']
             count +=1
-            print(count,number, name, address, status, lat, lng)
+            #print(count,number, name, address, status, lat, lng)
+        print("Scheduler called scrape method", datetime.datetime.now())
 
         #pprint.pprint(parsed_json)
         #print(len(parsed_json))
