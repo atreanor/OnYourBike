c517838d70fe326cdbb538b2b1f528abdb4d2aa2
SheenaDub <sheenadavitt@gmail.com>
2018-03-22 17:33:02 +0000

    Updated version of databaser - now writes to EC2

--- OnYourBike/databaser.py
+++ OnYourBike/databaser.py
@@ -1,4 +1,7 @@
 import MySQLdb
+import scraper
+import time
+import datetime
 
 def main():
     x = Databaser()
@@ -8,25 +11,24 @@ def main():
 class Databaser:
     
     def __init__(self):
-        self.db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                         user="root",         # your username
-                         passwd="root",  # your password
-                         db="mydb")        # name of the data base
+        self.db = MySQLdb.connect(host="onyourbikemysql.ccljfz7hpfu8.us-west-2.rds.amazonaws.com",    # your host, usually localhost
+                         user="Admin",         # your username
+                         passwd="UCD_2018",  # your password
+                         db="OnYourBikeMySQL")        # name of the data base
         self.cur = self.db.cursor()
         
     def createstatictable(self):
         self.cur.execute("DROP TABLE IF EXISTS static")
-        # Create table as per requirement
         self.sql = """CREATE TABLE static (number INT,
         contract_name CHAR(20),
         name CHAR(20),
         address CHAR(20),
-        lat INT,
-        lon INT,
+        lat FLOAT,
+        lng FLOAT,
         banking CHAR(20),
         bonus CHAR(20)        )"""
         self.cur.execute(self.sql) 
-        
+
         
     def inserter_static(self, a, b, c, d, e, f, g, h):
         self.num=a
@@ -37,31 +39,27 @@ class Databaser:
         self.lon=f
         self.bank=g
         self.bonus=h
-        # Prepare SQL query to INSERT a record into the database.
-        self.sql = "INSERT INTO static (number,\
-                 contract_name, name, address, lat, lon, banking, bonus) \
-               VALUES ('%d', '%s', '%s', '%s', '%d', '%d', '%s', '%s' )"  % \
+        self.sql = "INSERT INTO static_test (number,\
+                 contract_name, name, address, lat, lng, banking, bonus) \
+               VALUES ('%d', '%s', '%s', '%s', '%f', '%f', '%d', '%d' )"  % \
                (self.num, self.con, self.name, self.add, self.lat, self.lon, self.bank, self.bonus) 
         try:
-           # Execute the SQL command
            self.cur.execute(self.sql)
-           # Commit your changes in the database
            self.db.commit()
         except:
-           # Rollback in case there is any error
            self.db.rollback()
-        
+      
+    
         
     def createdynamictable(self):
-        self.cur.execute("DROP TABLE IF EXISTS dynamic1")
-        # Create table as per requirement
-        self.sql = """CREATE TABLE dynamic1 (
+        self.cur.execute("DROP TABLE IF EXISTS dynamic")
+        self.sql = """CREATE TABLE dynamic (
         number INT NOT NULL,
         status CHAR(10),
         bike_stands INT,
         available_bike_stands INT,
         available_bikes INT,
-        last_update TIMESTAMP
+        last_update DATETIME
         )"""
         self.cur.execute(self.sql) 
         
@@ -71,19 +69,18 @@ class Databaser:
         self.bikestands=c
         self.avail=d
         self.availbikes=e
-        self.last=f
-        # Prepare SQL query to INSERT a record into the database.
-        self.sql = """INSERT INTO dynamic1 (number,
+        self.last=f  
+        self.sql = "INSERT INTO dynamic_test (number, \
                  status, bike_stands, available_bike_stands, available_bikes, last_update) \
-               VALUES ('%d', '%s', '%d', '%d', '%d', '%s' )""" % \
-               (self.num, self.status, self.bikestands, self.avail, self.availbikes, self.last)
+               VALUES ('%d', '%s', '%d', '%d', '%d', '%s' )" % \
+               (self.num, self.status, self.bikestands, self.avail, self.availbikes, self.last)   
         try:
-           # Execute the SQL command
            self.cur.execute(self.sql)
-           # Commit your changes in the database
            self.db.commit()
+           return print("inserted into db")
         except:
-           # Rollback in case there is any error
            self.db.rollback()
                               
 main()
+
+
