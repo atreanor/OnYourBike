46558b9bbb616be8b4a2542ed9dc51e6e0decafc
SheenaDub <sheenadavitt@gmail.com>
2018-03-30 12:25:54 +0100

    changes to run.py (in flask) and to views.py, as well as html file friday.html which has javascript to run map

--- OYB_WebServer/app/views.py
+++ OYB_WebServer/app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from app import model
+
 @app.route('/')
 def index():
     returnDict = {}
@@ -10,3 +11,21 @@ def index():
     #apikey = "e8823ad03eaa6b4b5b80b84203e56c1740394008" # API key for JC Decaux
     #returnDict['owm'] = owm.owm_scrape()
     return render_template("index.html", **returnDict)
+    
+
+@app.route('/map')
+def map():
+    returnDict = {}
+    returnDict['name'] = 'Thomas Street'
+    returnDict['available_bikes'] = '20'
+    returnDict['free_stands'] = '13'
+    #returnDict['title'] = 'Home'
+    return render_template("friday.html", **returnDict)
+
+
+@app.route('/map2')
+def map2():
+    returnDict = {}
+    #returnDict['title'] = 'Home'
+    return render_template("friday2.html", **returnDict)
+
