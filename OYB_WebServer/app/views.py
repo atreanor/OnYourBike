f7057687f029ab8f31b67596e40d4aebf54887bf
SheenaDub <sheenadavitt@gmail.com>
2018-04-16 17:08:02 +0100

    removed config details

--- OYB_WebServer/app/views.py
+++ OYB_WebServer/app/views.py
@@ -16,11 +16,7 @@ from sqlalchemy import create_engine
 #from config import *  
 
 
-USER="Admin"
-PASSWORD="UCD_2018"
-URI="onyourbikemysql.cquggrydnjcx.eu-west-1.rds.amazonaws.com"
-PORT="3306"
-DB = "onyourbikemysql"
+
 
 
 
