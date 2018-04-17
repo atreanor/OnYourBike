e9c22f58057ec6691a8a904b4f14fc3a6d28df9c
Alan Treanor <alan.treanor@ucdconnect.ie>
2018-04-17 13:28:12 +0100

    Update getGraphData to accept selected station no. for MySQL statement

--- OYB_WebServer/app/views.py
+++ OYB_WebServer/app/views.py
@@ -62,13 +62,13 @@ def getweather():
     return jsonify(weather=weather)
 
 
-@app.route('/{GraphData}')
-def getGraphData():
+@app.route("/current/<int:number>")
+def getGraphData(number):
     ''' method to retrieve graph info for station selected on map, station number will be 
     passed as an argument into SQL statement to retrieve data specific to that station '''
     engine = get_db()
     graphData = []
-    rows = engine.execute("SELECT available_bike_stands, available_bikes, FROM JCD_dynamic_data, WHERE number={};")
+    rows = engine.execute("SELECT available_bike_stands, available_bikes, FROM JCD_dynamic_data, WHERE number={};".format(number))
     for row in rows:
         graphData.append(dict(row))
     return jsonify(graphData = graphData)
